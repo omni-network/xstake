bbe6e5745e3eabe22564700f21659509e053541b
Dennis <dennis@omni.network>
2024-04-06 18:43:56 +0200

    chore(token): natspec

--- src/LocalToken.sol
+++ src/LocalToken.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.8.25;
 
 import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
+/// @title LocalToken Contract
+/// @dev A contract for the ERC20 token on the rollup chain.
 contract LocalToken is ERC20 {
+    /// @dev Constructor function that initializes the LocalToken contract.
     constructor() ERC20("LocalToken", "LT") {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
