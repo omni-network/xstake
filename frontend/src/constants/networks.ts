d0652f00cc7793e404b4f91962bfcef840ee370b
Dennis <dennis@omni.network>
2024-04-09 15:52:39 +0200

    feat(frontend): add chain ID to networks

--- frontend/src/constants/networks.ts
+++ frontend/src/constants/networks.ts
@@ -2,6 +2,7 @@
 interface Network {
   name: string;
   rpcUrl: string;
+  chainId?: number;
   stakeContractAddress?: string;
   localTokenContractAddress?: string;
   globalManagerContractAddress?: string;
@@ -15,17 +16,20 @@ export const networks: Networks = {
   omni: {
     name: "Omni",
     rpcUrl: "http://localhost:8000",
+    chainId: 16561,
     globalManagerContractAddress: "0xb7278A61aa25c888815aFC32Ad3cC52fF24fE575",
   },
   op: {
     name: "Optimism",
     rpcUrl: "http://localhost:8002",
+    chainId: 100,
     localTokenContractAddress: "0x4C2F7092C2aE51D986bEFEe378e50BD4dB99C901",
     stakeContractAddress: "0x7A9Ec1d04904907De0ED7b6839CcdD59c3716AC9",
   },
   arb: {
     name: "Arbitrum",
     rpcUrl: "http://localhost:8003",
+    chainId: 200,
     localTokenContractAddress: "0x4c5859f0F772848b2D91F1D83E2Fe57935348029",
     stakeContractAddress: "0x1291Be112d480055DaFd8a610b7d1e203891C274",
   }
