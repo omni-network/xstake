a97c58649cefe6618412a7d7498c8fabf9162441
Dennis <dennis@omni.network>
2024-04-11 13:28:12 +0200

    refactor(frontend): first nonce deploy addresses

--- script/bash/env.sh
+++ script/bash/env.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-export PORTAL_ADDRESS=0xf4060C06cAD9Dd22dc520d04c0a2E139303091CA # testnet portal address (change it for other networks)
+export PORTAL_ADDRESS=0xb0aE5EDFE755E7AE3bE21370dfAC627C79881384 # testnet portal address (change it for other networks)
 export GLOBAL_CHAIN_ID=16561 # testnet global chain ID is 165
-export OMNI_RPC_URL=http://localhost:8000
+export OMNI_RPC_URL=http://localhost:8001
 export OP_CHAIN_ID=100 # Optimism Sepolia Chain ID is 11155420
 export OP_RPC_URL=http://localhost:8002
 export ARB_CHAIN_ID=200 # Arbitrum Sepolia Chain ID is 421614
