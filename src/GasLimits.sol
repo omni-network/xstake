41fc317c38aa80c6b650178110cc51d8bec6c0c8
Dennis <dennis@omni.network>
2024-04-29 18:15:09 +0200

    fix(contract): add gas usage from test as comments in abstract contract

--- src/GasLimits.sol
+++ src/GasLimits.sol
@@ -12,17 +12,17 @@ abstract contract GasLimits {
      * @notice Maximum gas used for delegating unstaking operations via cross-chain communication.
      * @dev This value is derived from test results in LocalStakeTest.testXUnstakeGasProfile().
      */
-    uint64 public constant XUNSTAKE_GAS = 90_000;
+    uint64 public constant XUNSTAKE_GAS = 90_000; // test xunstake gas used: 77556
 
     /**
      * @notice Maximum gas limit for adding stake in cross-chain operations.
      * @dev This value is derived from test results in GlobalManagerTest.testAddStakeProfileGas().
      */
-    uint64 public ADD_STAKE_GAS = 110_000;
+    uint64 public ADD_STAKE_GAS = 110_000; // test addStake gas used: 99688
 
     /**
      * @notice Maximum gas limit for removing stake in cross-chain operations.
      * @dev This value is derived from test results in GlobalManagerTest.testRemoveStakeProfileGas().
      */
-    uint64 public REMOVE_STAKE_GAS = 90_000;
+    uint64 public REMOVE_STAKE_GAS = 90_000; // test removeStake gas used: 74799
 }
