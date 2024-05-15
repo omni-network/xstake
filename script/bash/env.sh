7b58458b5aa122ebc455921a4d05b1caafafbd7d
Dennis <dennis@omni.network>
2024-05-15 14:41:15 +0200

    chore(script): make work with devnet0

--- script/bash/env.sh
+++ script/bash/env.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 export PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 # anvil first private key
-export PORTAL_ADDRESS=0x505cd0cf0121c790812d5f3f08585d548236e605 # portal address
-export GLOBAL_CHAIN_ID=16561 # global chain ID (testnet is 165)
+export PORTAL_ADDRESS=0x0c2764f3726c0cfa9d84687d1bf8ddceb4835a9f # portal address
+export GLOBAL_CHAIN_ID=1651 # global chain ID (testnet is 165)
 export OMNI_RPC_URL=http://localhost:8000 # Omni EVM RPC URL
-export OP_CHAIN_ID=100 # Optimism Sepolia Chain ID is 11155420
+export OP_CHAIN_ID=1655 # Optimism Sepolia Chain ID is 11155420
 export OP_RPC_URL=http://localhost:8001 # Optimism RPC URL
-export ARB_CHAIN_ID=200 # Arbitrum Sepolia Chain ID is 421614
+export ARB_CHAIN_ID=1656 # Arbitrum Sepolia Chain ID is 421614
 export ARB_RPC_URL=http://localhost:8002 # Arbitrum RPC URL
